91e1a2a044e2d157e4d47b64d02ae3537a80897b
Sukanya <Sukanyak21130@gmail.com>
2017-10-22 20:34:02 +0700

    W09P22

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -20,7 +20,28 @@ namespace WindowsFormsApp1
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-          
+            Graphics g = e.Graphics;
+            Bitmap bmp = new Bitmap("D:\\nann.Png");
+            this.SetClientSizeCore(bmp.Width, bmp.Height);
+            Rectangle topleft = new Rectangle(0, 0, bmp.Width / 2, bmp.Height / 2);
+            Rectangle topRight = new Rectangle(bmp.Width / 2, 0, bmp.Height / 2, bmp.Height / 2);
+            Rectangle bottomLeft = new Rectangle(0, bmp.Width / 2, bmp.Height / 2, bmp.Height / 2);
+            Rectangle bottomRight = new Rectangle(bmp.Width / 2, bmp.Height / 2, bmp.Width / 2, bmp.Height);
+
+            bmp.RotateFlip(RotateFlipType.RotateNoneFlipNone);
+            e.Graphics.DrawImage(bmp, topleft);
+            bmp.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            e.Graphics.DrawImage(bmp, topRight);
+
+            bmp.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            e.Graphics.DrawImage(bmp, bottomLeft);
+
+            bmp.RotateFlip(RotateFlipType.Rotate180FlipY);
+            e.Graphics.DrawImage(bmp, bottomRight);
+
+            g.Dispose();
+
+
 
 
         }
