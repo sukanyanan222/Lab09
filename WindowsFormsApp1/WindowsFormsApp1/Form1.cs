cc87984a84477b2a1f41198c9dd87bc64ceaabde
Sukanya <Sukanyak21130@gmail.com>
2017-10-22 19:22:14 +0700

    W09P18

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -20,7 +20,27 @@ namespace WindowsFormsApp1
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-          
+            this.SetClientSizeCore(500, 600);
+            Graphics g = e.Graphics;
+            System.Drawing.Drawing2D.HatchBrush brush;
+            int x = 20;
+            int y = 20;
+            foreach (System.Drawing.Drawing2D.HatchStyle brushStyle in Enum.GetValues(typeof(System.Drawing.Drawing2D.HatchStyle)))
+            {
+                brush = new System.Drawing.Drawing2D.HatchBrush(brushStyle, Color.Navy, Color.Yellow);
+                g.FillRectangle(brush, x, y, 40, 20);
+                y += 30;
+                if ((y + 30) > this.ClientSize.Height)
+                {
+                    y = 20;
+                    x += 150;
+
+
+                }
+
+            }
+            g.Dispose();
+
 
 
         }
