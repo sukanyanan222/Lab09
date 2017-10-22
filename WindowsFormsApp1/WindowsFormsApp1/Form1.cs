5e08f0033f5d793cb284460659a41febf8ac54ba
Sukanya <Sukanyak21130@gmail.com>
2017-10-22 18:23:30 +0700

    W09P13

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -20,7 +20,16 @@ namespace WindowsFormsApp1
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            
+            Graphics g = e.Graphics;
+            Pen myPen = new Pen(Color.Blue, 3);
+            Rectangle[] rects = {
+                new Rectangle(20,20,120,20),
+                new Rectangle(20,50,120,30),
+                new Rectangle(20,90,120,40),
+                new Rectangle(20,140,120,60), };
+            g.DrawRectangles(myPen, rects);
+            g.Dispose();
+
 
         }
     }
