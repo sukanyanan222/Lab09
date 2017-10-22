c4d481f291a9dd7a58247b2a04a52698c7cb3c9a
Sukanya <Sukanyak21130@gmail.com>
2017-10-22 18:17:29 +0700

    W09P11

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -20,7 +20,26 @@ namespace WindowsFormsApp1
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            
+            Graphics g = e.Graphics;
+            System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
+            gp.AddCurve(new Point[] {
+                 new Point(100,50),
+                 new Point(105,40),
+                 new Point(120,40),
+                 new Point(130,65),
+                 new Point(100,100)
+            }, 0.5f);
+            gp.AddCurve(new Point[] {
+                 new Point(100,100),
+                 new Point(70,65),
+                 new Point(80,40),
+                 new Point(95,40),
+                 new Point(100,50)
+            }, 0.5f);
+
+            g.DrawPath(new Pen(Color.Red, 3), gp);
+            g.Dispose();
+
 
         }
     }
