0eb22742fc220d63278a004a660f4f0cc2b19ac2
Sukanya <Sukanyak21130@gmail.com>
2017-10-22 17:54:45 +0700

    W09P02

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -20,7 +20,19 @@ namespace WindowsFormsApp1
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            
+            Graphics g = e.Graphics;
+            Pen bluepen = new Pen(Color.Blue, 2);
+            g.DrawRectangle(bluepen, 10, 10, 100, 100);
+            bluepen.Dispose();
+
+            Pen somepen = new Pen(Color.FromArgb(255, 120, 200));
+            g.DrawEllipse(somepen, 20, 20, 200, 200);
+            somepen.Dispose();
+
+            Color col = Color.FromName("LightGreen");
+            this.BackColor = col;
+
+
 
         }
     }
